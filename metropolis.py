fcd48ec66f417b2cf5c35aec5b3ca5ae5bedc576
dpalmer4 <60151424+dpalmer4@users.noreply.github.com>
2020-06-01 15:13:34 -0500

    Update metropolis.py

--- metropolis.py
+++ metropolis.py
@@ -28,23 +28,22 @@ def gaussian_dist(x):
     g2=gaussian(var2,ave2,x)
     return g1+g2
 
-def metropolis(steps,desired_dist):
+def metropolis(steps,desired_dist,randoms):
     """metrolpolis algorithim in 1d given number of samples
     outputs markov chain, chain of average of x, chain of variance of x,acceptance fraction"""
     x=np.zeros(steps+1)
     x[0]=.5
     ave_x=np.zeros(steps)
     var_x=np.zeros(steps)
-    randoms=np.random.normal(size=steps)
     accepted=0
-    
+    rands=np.random.rand(steps)
     for i in range(steps):
         x_next=x[i]+randoms[i]
         if (desired_dist(x_next)/desired_dist(x[i]))>=1:
             x[i+1]=x_next
             accepted+=1
         else:
-            if np.random.rand()<=(desired_dist(x_next)/desired_dist(x[i])):
+            if rands[i]<=(desired_dist(x_next)/desired_dist(x[i])):
                 x[i+1]=x_next
                 accepted+=1
             else:
@@ -55,13 +54,26 @@ def metropolis(steps,desired_dist):
     return [x,ave_x,var_x,acceptance_fraction]
 
 #plotting histogram of metropolis output, average of x vs. step, variance of x vs. step, acceptance fraction
-steps=100000   
-[x,ave_x,var_x,acceptance_fraction]=metropolis(steps,gaussian_dist)
+steps=20000 
+####algorithim check
+check_steps=100
+xlowlim=0
+xuplim=1
+c=np.linspace(xlowlim,xuplim,check_steps)
+cx=np.zeros(check_steps)
+for i in range(check_steps): 
+    cx[i]=linear_dist(c[i])
+    
+    
+randoms=np.random.normal(size=steps)
+[x,ave_x,var_x,acceptance_fraction]=metropolis(steps,linear_dist,randoms)
 t=range(steps)
-plt.hist(x,bins=100,density=1)
+plt.hist(x,bins=100,density=1,label="metropolis distribution")
+plt.plot(c,cx,label="predicted distribution")
 plt.ylabel("probability")
 plt.xlabel("x")
 print("acceptance fraction= ",acceptance_fraction)
+plt.legend()
 plt.show()
 plt.plot(t,ave_x)
 plt.ylabel("average of x")
